c645beb8ae855a3fd325a7d6a10033211730c8c3
lenverbij <61278357+lenverbij@users.noreply.github.com>
2020-03-03 15:02:14 +0100

    Dashboard definitief
    Dashboard definitief (voor test)

---
+++ mainscript.js
@@ -0,0 +1,224 @@
+function start(){
+    functionDistance();
+    functionSpeed();
+    functionAcceleration();
+	functionGravity();
+}
+
+window.onload = start(); 
+
+function functionDistance(){
+    
+    //Dit is de code voor de afstand
+    var distanceChart = document.getElementById('distance').getContext('2d');
+
+    Chart.defaults.global.defaultFontSize = 16;
+    Chart.defaults.global.defaultFontColor = '#fff';
+
+    var massPopChart = new Chart(distanceChart, {
+      type:'horizontalBar',
+      data:{
+        labels:['Shuttle'],
+        datasets:[{
+          label:'KM',
+          data:[
+            120//dit is de huide afstand van de shuttle.
+          ],
+          backgroundColor:[
+            '#FFFFFF'
+          ],
+        }]
+      },
+      options:{
+        title:{
+          display:true,
+		  text:'Kilometers (x1000)'
+        },
+        scales:{
+          xAxes: [{
+            display: true,
+            ticks: {
+                beginAtZero: true,
+                steps: 100,
+                stepValue: 5,
+                max: 400
+            }
+        }]
+        },
+        legend:{
+          display:false,
+          position:'right',
+          labels:{
+            fontColor:'#000'
+          }
+        },
+        tooltips:{
+          enabled:true
+        }
+      }
+    });
+    
+    
+}
+
+function functionSpeed(){
+    
+    //Dit is de code voor de snelheid
+    
+    var speedChart = document.getElementById('speed').getContext('2d');
+
+    Chart.defaults.global.defaultFontSize = 16;
+    Chart.defaults.global.defaultFontColor = '#fff';
+
+    var massPopChart = new Chart(speedChart, {
+      type:'doughnut',
+      data:{
+        labels:['Current-speed', 'Untill max-speed'],
+        datasets:[{
+          label:'km/h',
+          data:[
+            13678,//Dit side snelheid die je kan aanpassen
+            6322//max-snelheid in km/h
+          ],
+          backgroundColor:[
+            '#FFF',
+            '#000'
+          ],
+        }]
+      },
+      options:{
+		   title:{
+          display:true,
+		  text:'Speed in Km/h (max 20000)'
+        },
+        legend:{
+          display:true,
+          position:'right',
+          labels:{
+            fontColor:'#FFF'
+          }
+        },
+        rotation: 1 * Math.PI,
+        circumference: 1 * Math.PI
+      }
+    });
+    
+}
+
+function functionAcceleration(){
+    
+    //Dit is de code voor de G kracht
+    var accelerationChart = document.getElementById('acceleration').getContext('2d');
+
+    Chart.defaults.global.defaultFontSize = 16;
+    Chart.defaults.global.defaultFontColor = '#fff';
+
+    var massPopChart = new Chart(accelerationChart, {
+      type:'line',
+      data:{
+        labels:[0,100,200,300,400,500,600,700,800],
+        datasets:[{
+          label:'',
+          data:[
+              
+            // G kracht van momenten tijdens de vlucht. voel vrij om de waardes aan te passen.
+            10,
+            37,
+            39,
+            0,
+			6,
+			16,
+			15,
+			0,
+			5,
+          ],
+          borderWidth:1,
+          borderColor:'#fff',
+          backgroundColor:[
+			'rgba(224, 224, 224,0.5)',
+			'rgba(224, 224, 224,0.5)',
+			'rgba(224, 224, 224,0.5)',
+			'rgba(224, 224, 224,0.5)',
+			'rgba(224, 224, 224,0.5)',
+			'rgba(224, 224, 224,0.5)',
+			'rgba(224, 224, 224,0.5)',
+			'rgba(224, 224, 224,0.5)',
+			'rgba(224, 224, 224,0.5)',
+          ]
+        }]
+      },
+      options:{
+        title:{
+          display:true,
+		  text: 'Meters/second',
+        },
+        scales:{
+          yAxes: [{
+            display: true,
+            ticks: {
+                beginAtZero: true,
+                steps: 5,
+                max: 40
+            }
+          }]
+        },
+        legend:{
+          display:false
+        },
+        tooltips:{
+          enabled:true
+        }
+      }
+    });   
+}
+
+
+function functionGravity(){
+    
+    //Dit is de code voor de zwaartekracht
+    
+    var gravityChart = document.getElementById('gravity').getContext('2d');
+
+    Chart.defaults.global.defaultFontSize = 16;
+    Chart.defaults.global.defaultFontColor = '#fff';
+
+    var massPopChart = new Chart(gravityChart, {
+      type:'bar',
+      data:{
+        labels:['Aarde', 'Shuttle', 'Mars'],
+        datasets:[{
+          label:'Newton',
+          data:[
+            9.807,
+            1.201,//Waarde van de shuttle. Je kan het veranderen
+            3.711
+          ],
+          backgroundColor:[
+            '#00bcd4',
+            '#FFF',
+            '#ffca7e'
+          ]
+        }]
+      },
+      options:{
+        title:{
+          display:true,
+          text:'Zwaartekracht in Newton'
+        },
+        scales:{
+          yAxes: [{
+            display: true,
+            ticks: {
+                beginAtZero: true
+            }
+        }]
+        },
+        legend:{
+          display:false
+        },
+        tooltips:{
+          enabled:true
+        }
+      }
+    });
+}
